ecfa88d0f771d0a65defdafb621e815f1dc1923c
David Gauchard <gauchard@laas.fr>
2018-04-09 11:51:55 +0200

    update api

--- transfer.h
+++ transfer.h
@@ -6,28 +6,39 @@
 #include <assert.h>
 
 #define transferTo(i,o)			_transferTo<__typeof__(i),__typeof__(o)>(i,o)
-#define transferToMax(i,o,ms)		_transferTo<__typeof__(i),__typeof__(o)>(i,o,ms)
+#define transferToMax(i,o,maxsize)	_transferTo<__typeof__(i),__typeof__(o)>(i,o,maxsize)
 #define setTransferMaxBufSize(s)	(_transfer_maxsize = (s))
 
-#define USTREAMBUFRNDBITS		4 // round bufsize to multiple of 2^4
-#define USTREAMBUFRND			(1<<USTREAMBUFRNDBITS)
-#define USTREAMBUFSZ(x)			(((x) + USTREAMBUFRND - 1) & ~(USTREAMBUFRND - 1))
+#define TRANSFERBUFRNDBITS		4 // round bufsize to multiple of 2^4
+#define TRANSFERBUFRND			(1<<TRANSFERBUFRNDBITS)
+#define TRANSFERBUFSZ(x)		(((x) + (TRANSFERBUFRND) - 1) & ~((TRANSFERBUFRND) - 1))
+
+#ifndef TRANSFERBUFABSMAXSIZE
+#ifndef TCP_MSS
+#warning TCP_MSS is undefined
+#define TRANSFERBUFABSMAXSIZE		(1460)
+#else // defined(TCP_MSS)
+#define TRANSFERBUFABSMAXSIZE		(TCP_MSS)
+#endif // defined(TCP_MSS)
+#endif // !defined(TRANSFERBUFABSMAXSIZE)
 
 size_t _transferTo_bufsize __attribute__((weak)) = 0;
 char*  _transferTo_buf     __attribute__((weak)) = 0;
 
-size_t _transferTo_size (size_t availr, size_t availw, ssize_t maxsize = -1) __attribute__((weak, warn_unused_result));
-size_t _transferTo_size (size_t availr, size_t availw, ssize_t maxsize)
+size_t transferTo_alloc (size_t size1, size_t size2, size_t maxsize = 0) __attribute__((weak, warn_unused_result));
+size_t transferTo_alloc (size_t size1, size_t size2, size_t maxsize)
 {
-	if (availr > availw)
-		availr = availw;
-	if (maxsize >= 0 && availr > (size_t)maxsize)
-		availr = maxsize;
-	if (!availr)
+	if (size1 > size2)
+		size1 = size2;
+	if (maxsize && size1 > maxsize)
+		size1 = maxsize;
+	if (size1 > TRANSFERBUFABSMAXSIZE)
+		size1 = TRANSFERBUFABSMAXSIZE;
+	if (!size1)
 		return 0;
-	if (availr > _transferTo_bufsize)
+	if (size1 > _transferTo_bufsize)
 	{
-		size_t newsize = USTREAMBUFSZ(availr);
+		size_t newsize = TRANSFERBUFSZ(size1);
 		char* newbuf = (char*)realloc(_transferTo_buf, newsize);
 		if (newbuf)
 		{
@@ -35,25 +46,18 @@ size_t _transferTo_size (size_t availr, size_t availw, ssize_t maxsize)
 			_transferTo_bufsize = newsize;
 		}
 		else
-			availr = _transferTo_bufsize;
+			size1 = _transferTo_bufsize;
 		//os_printf(":strm %d\n", _transferTo_bufsize);
 		if (!_transferTo_bufsize)
 			return 0;
 	}
-	return availr;
-}
-
-void _transferTo_alloc (size_t size) __attribute__((weak));
-void _transferTo_alloc (size_t size)
-{
-	size_t ign = _transferTo_size(size, size);
-	(void)sizeof(ign);
+	return size1;
 }
 
 template <class SI, class SO>
-size_t _transferTo (SI& in, SO& out, ssize_t maxsize = -1)
+size_t _transferTo (SI& in, SO& out, ssize_t maxsize = 0)
 {
-	size_t size = _transferTo_size(in.available(), out.availableForWrite(), maxsize);
+	size_t size = transferTo_alloc(in.available(), out.availableForWrite(), maxsize);
 	if (!size)
 		return 0;
 	size = in.readBytes(_transferTo_buf, size);
